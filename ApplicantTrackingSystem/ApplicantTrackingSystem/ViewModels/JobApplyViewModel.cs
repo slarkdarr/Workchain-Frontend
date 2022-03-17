2643c1e74495e8826c25ab43e06bcf804217bd3a
myahyaibrahim <yahyaibrahim01@gmail.com>
2022-03-17 20:13:25 +0700

    Add model for API

--- ApplicantTrackingSystem/ApplicantTrackingSystem/ViewModels/JobApplyViewModel.cs
+++ ApplicantTrackingSystem/ApplicantTrackingSystem/ViewModels/JobApplyViewModel.cs
@@ -1,4 +1,6 @@
-﻿using MvvmHelpers.Commands;
+﻿using ApplicantTrackingSystem.Models;
+using ApplicantTrackingSystem.Services;
+using MvvmHelpers.Commands;
 using Npgsql;
 using System;
 using System.Collections.Generic;
@@ -6,6 +8,7 @@ using System.ComponentModel;
 using System.Text;
 using Xamarin.Forms;
 using Command = MvvmHelpers.Commands.Command;
+using System.Globalization;
 
 namespace ApplicantTrackingSystem.ViewModels
 {
@@ -16,6 +19,9 @@ namespace ApplicantTrackingSystem.ViewModels
         public Command SubmitCommand { get; }
         public JobApplyViewModel()
         {
+            DateTime localDate = DateTime.Now;
+            ApplyDate = localDate;
+
             SubmitCommand = new Command(Submit);
         }
 
@@ -77,40 +83,56 @@ namespace ApplicantTrackingSystem.ViewModels
 
         async void Submit()
         {
-            var connString = "Host=ec2-3-219-204-29.compute-1.amazonaws.com;Database=d7p6gej9knqefg;Username=ptyxepvslwevdw;Password=2cff69469572cf04b3e738727d1503ccd0e05efc9b1d73f9ac6061954f094771";
-
-            await using var conn = new NpgsqlConnection(connString);
-
-            Console.WriteLine("Opening");
-            await conn.OpenAsync();
-
-            Console.WriteLine("Opened");
-
-
-            string query = "INSERT INTO job_application (job_id, applicant_id, apply_date, requirement_link, status, applicant_name, applicat_email, applicant_telp) VALUES (@job_id, @applicant_id, @apply_date, @requirement_link, @status, @applicant_name, @applicat_email, @applicant_telp)";
-            try
+            var jobApplication = new JobApplication
             {
-                using (var cmd1 = new NpgsqlCommand(query, conn))
-                {
-                    cmd1.Parameters.AddWithValue("job_id", JobId);
-                    cmd1.Parameters.AddWithValue("applicant_id", ApplicantId);
-                    cmd1.Parameters.AddWithValue("apply_date", ApplyDate);
-                    cmd1.Parameters.AddWithValue("requirement_link", RequirementLink);
-                    cmd1.Parameters.AddWithValue("status", Status);
-                    cmd1.Parameters.AddWithValue("applicant_name", ApplicantName);
-                    cmd1.Parameters.AddWithValue("applicat_email", ApplicantEmail);
-                    cmd1.Parameters.AddWithValue("applicant_telp", ApplicantTelp);
-                    await cmd1.ExecuteNonQueryAsync();
-                };
-                Console.WriteLine("Successfully inserted new job application");
-            }
+                job_id = jobId,
+                apply_date = applyDate.ToString("dd/MM/yyyy"),
+                requirement_link = requirementLink,
+                status = status,
+                applicant_name = applicantName,
+                applicant_email = applicantEmail,
+                applicant_telp = applicantTelp,
+            };
 
-            catch (Exception ex)
-            {
-                Console.WriteLine(ex);
-                conn.Close();
-            }
-
+            await AtsService.AddJobApplication(jobApplication);
         }
+
+        //async void Submit()
+        //{
+        //    var connString = "Host=ec2-3-219-204-29.compute-1.amazonaws.com;Database=d7p6gej9knqefg;Username=ptyxepvslwevdw;Password=2cff69469572cf04b3e738727d1503ccd0e05efc9b1d73f9ac6061954f094771";
+
+        //    await using var conn = new NpgsqlConnection(connString);
+
+        //    Console.WriteLine("Opening");
+        //    await conn.OpenAsync();
+
+        //    Console.WriteLine("Opened");
+
+
+        //    string query = "INSERT INTO job_application (job_id, applicant_id, apply_date, requirement_link, status, applicant_name, applicat_email, applicant_telp) VALUES (@job_id, @applicant_id, @apply_date, @requirement_link, @status, @applicant_name, @applicat_email, @applicant_telp)";
+        //    try
+        //    {
+        //        using (var cmd1 = new NpgsqlCommand(query, conn))
+        //        {
+        //            cmd1.Parameters.AddWithValue("job_id", JobId);
+        //            cmd1.Parameters.AddWithValue("applicant_id", ApplicantId);
+        //            cmd1.Parameters.AddWithValue("apply_date", ApplyDate);
+        //            cmd1.Parameters.AddWithValue("requirement_link", RequirementLink);
+        //            cmd1.Parameters.AddWithValue("status", Status);
+        //            cmd1.Parameters.AddWithValue("applicant_name", ApplicantName);
+        //            cmd1.Parameters.AddWithValue("applicat_email", ApplicantEmail);
+        //            cmd1.Parameters.AddWithValue("applicant_telp", ApplicantTelp);
+        //            await cmd1.ExecuteNonQueryAsync();
+        //        };
+        //        Console.WriteLine("Successfully inserted new job application");
+        //    }
+
+        //    catch (Exception ex)
+        //    {
+        //        Console.WriteLine(ex);
+        //        conn.Close();
+        //    }
+
+        //}
     }
 }
