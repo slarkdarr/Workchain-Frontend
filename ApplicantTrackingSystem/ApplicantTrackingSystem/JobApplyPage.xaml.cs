cd8eb4f446c4476b4e96e0ca4610a9140c09c893
salsabiilashifa11 <13519106@std.stei.itb.ac.id>
2022-03-21 13:11:00 +0700

    Readded job apply fe pages

---
+++ ApplicantTrackingSystem/ApplicantTrackingSystem/JobApplyPage.xaml.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+
+using Xamarin.Forms;
+
+namespace ApplicantTrackingSystem
+{
+    public partial class JobApplyPage : ContentPage
+    {
+        public JobApplyPage()
+        {
+            InitializeComponent();
+        }
+
+        private void FrameFocusedReg(object sender, FocusEventArgs e)
+        {
+            var entry = (Entry)sender;
+            var classId = entry.ClassId;
+            var identifier = classId + "Frame";
+
+            var element = (Frame)applyCard.FindByName(identifier);
+
+            element.BorderColor = Color.FromHex("#58327F");
+        }
+
+        private void FrameUnfocusedReg(object sender, FocusEventArgs e)
+        {
+            var entry = (Entry)sender;
+            var classId = entry.ClassId;
+            var identifier = classId + "Frame";
+
+            var element = (Frame)applyCard.FindByName(identifier);
+
+            element.BorderColor = Color.Transparent;
+        }
+
+        private void FrameFocusedEditor(object sender, FocusEventArgs e)
+        {
+            var entry = (Editor)sender;
+            var classId = entry.ClassId;
+            var identifier = classId + "Frame";
+
+            var element = (Frame)applyCard.FindByName(identifier);
+
+            element.BorderColor = Color.FromHex("#58327F");
+        }
+
+        private void FrameUnfocusedEditor(object sender, FocusEventArgs e)
+        {
+            var entry = (Editor)sender;
+            var classId = entry.ClassId;
+            var identifier = classId + "Frame";
+
+            var element = (Frame)applyCard.FindByName(identifier);
+
+            element.BorderColor = Color.Transparent;
+        }
+
+        private void Submit(object sender, EventArgs e)
+        {
+            //var vm = new JobVacancyPostViewModel();
+            //this.BindingContext = vm;
+            //StartDate.Date = DateTime.Now;
+
+            //vm.SubmitCommand.Execute(null);
+        }
+    }
+}
